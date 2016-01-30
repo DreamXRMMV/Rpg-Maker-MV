9784b4f6e635323894e7c9512eb17b11367f7fd0
DreamXRMMV <faewfbaiobthwbrw5235@outlook.com>
2016-01-29 17:34:47 -0800

    v1.02 - Party members that have <no_menu:1> (or <no_menu_switch:x> if switch x is on) will not appear in menus.

--- DreamX_FollowerOptions.js
+++ DreamX_FollowerOptions.js
@@ -1,5 +1,5 @@
 /*:
- * @plugindesc v1.01 Choose which party members appear as followers or in battle.
+ * @plugindesc v1.02 Choose which party members appear as followers or in battle.
  *
  * <DreamX Follower and Battle Member Options>
  * @author DreamX
@@ -37,14 +37,13 @@
  * determines whether they're a follower. Change x to the switch number. 
  * You must use one of the following plugin commands to alter follower visiblity: 
  * ToggleFollower 2 	- Toggles the visiblity of an actor as a follower.
- *						Use the actor id. In this case, it is actor #2.
+ *			Use the actor id. In this case, it is actor #2.
  * ShowFollower 4 	- Shows actor as follower
- *						Use the actor id. In this case, it is actor #4.
+ *			Use the actor id. In this case, it is actor #4.
  * HideFollower  6 	- Hides actor as follower
- *						Use the actor id. In this case, it is actor #6.
+ *			Use the actor id. In this case, it is actor #6.
  * Do not use both follower notetags at the same time for the same actor, 
  * choose one or  the other.
- *
  *
  * Use <no_battle:1> in an actor's notetag to permanently prevent them from 
  * appearing in battle. Use <no_battle_switch:x> in an actor's notetag to 
@@ -53,10 +52,22 @@
  * If it's false, they will. Do not use both notetags at the same time for the 
  * same actor, choose one or the other. You don't need to use plugin commands 
  * when using the switch notetag for battle.
+ * 
+ * Use <no_menu:1> in an actor's notetag to permanently prevent them from 
+ * appearing in menus. Use <no_menu_switch:x> in an actor's notetag to 
+ * designate a switch as what determines whether they appear in menus. Change x 
+ * to the switch number. If the switch is true, the actor won't appear in menus. 
+ * If it's false, they will. Do not use both notetags at the same time for the 
+ * same actor, choose one or the other. You don't need to use plugin commands 
+ * when using the switch notetag for menus.
  * ============================================================================
  * Terms Of Use
  * ============================================================================
  * Free to use and modify for commercial and noncommercial games, with credit.
+ * ============================================================================
+ * Compatibility
+ * ============================================================================
+ * To use Hime's Custom Party Leader, make sure to place it under this plugin.
  * ============================================================================
  * Credits
  * ============================================================================
@@ -64,44 +75,107 @@
  * Thanks to Shaz and Liquidize on the Rpg Maker forum for assistance.
  */
 
+//=============================================================================
+// Import
+//=============================================================================
 var Imported = Imported || {};
 Imported.DreamX_FollowerOptions = true;
 var DreamX = DreamX || {};
 DreamX.FollowerOptions = DreamX.FollowerOptions || {};
+
+
 (function () {
 
+//=============================================================================
+// Parameters
+//=============================================================================
     var parameters = $plugins.filter(function (p) {
-        return p.description.contains('<DreamX Follower and Battle Member Options>');
+        return p.description.contains
+                ('<DreamX Follower and Battle Member Options>');
     })[0].parameters; //Thanks to Iavra
 
     var parameterMaxIndex = parseInt(parameters['Max Index'] || '12');
-    var parameterBattleMembersOnly = String(parameters['Battle Members Only'] || 'true');
-    var parameterMaxFollowers = String(parameters['Max Followers'] || '$gameParty.maxBattleMembers() - 1');
+    var parameterBattleMembersOnly = String(parameters['Battle Members Only']
+            || 'true');
+    var parameterMaxFollowers = String(parameters['Max Followers']
+            || '$gameParty.maxBattleMembers() - 1');
+
+//=============================================================================
+// Alias/Override Functions
+//=============================================================================
+    //==========================================================================
+    // Game_Interpreter
+    //==========================================================================
     DreamX.FollowerOptions._Game_Interpreter_pluginCommand =
             Game_Interpreter.prototype.pluginCommand;
     Game_Interpreter.prototype.pluginCommand = function (command, args) {
-        DreamX.FollowerOptions._Game_Interpreter_pluginCommand.call(this, command, args);
+        DreamX.FollowerOptions._Game_Interpreter_pluginCommand.call(this,
+                command, args);
         if (args[0] && parseInt(args[0]) >= 1) {
-            var switchNum = DreamX.FollowerOptions.getSwitchNum(args[0]);
+            var argument = DreamX.FollowerOptions.getSwitchNum(args[0]);
             switch (command) {
                 case 'ToggleFollower':
-                    DreamX.FollowerOptions.ToggleFollower(switchNum);
+                    DreamX.FollowerOptions.ToggleFollower(argument);
                     $gamePlayer.followers().refresh();
                     break;
                 case 'ShowFollower':
-                    DreamX.FollowerOptions.FollowerOff(switchNum);
+                    DreamX.FollowerOptions.FollowerOff(argument);
                     $gamePlayer.followers().refresh();
                     break;
                 case 'HideFollower':
-                    DreamX.FollowerOptions.FollowerOn(switchNum);
+                    DreamX.FollowerOptions.FollowerOn(argument);
                     $gamePlayer.followers().refresh();
                     break;
             }
         }
-
     };
+
+    //==========================================================================
+    // Game_Party
+    //==========================================================================
+    DreamX.FollowerOptions.Game_Party_initialize
+            = Game_Party.prototype.initialize;
+    Game_Party.prototype.initialize = function () {
+        DreamX.FollowerOptions.Game_Party_initialize.call(this);
+        this._customFollowers = [];
+    };
+
+    DreamX.FollowerOptions.Game_Party_leader = Game_Party.prototype.leader;
+    Game_Party.prototype.leader = function () {
+        if (eval(parameterBattleMembersOnly) === false) {
+            return this.allPartyMembers()[0];
+        }
+        return DreamX.FollowerOptions.Game_Party_leader.call(this);
+    };
+
+    Game_Party.prototype.allMembers = function () {
+        var members = [];
+        this._actors.forEach(function (id) {
+            if (DreamX.FollowerOptions.isActorMenuEnabled
+                    ($gameActors.actor(id))) {
+                members.push($gameActors.actor(id));
+            }
+        });
+        return members;
+    };
+
+    Game_Party.prototype.battleMembers = function () {
+        var battleMembers = this.allMembers().filter(function (actor) {
+            return actor.isAppeared()
+                    && DreamX.FollowerOptions.isActorBattleEnabled(actor);
+        });
+        return battleMembers.slice(0, this.maxBattleMembers());
+    };
+
+//=============================================================================
+// Original Functions
+//=============================================================================
+    //==========================================================================
+    // DreamX.FollowerOptions
+    //==========================================================================
     DreamX.FollowerOptions.getSwitchNum = function (actorId) {
-        var switchNum = $dataActors[actorId].meta.no_follow_switch ? $dataActors[actorId].meta.no_follow_switch : -1;
+        var switchNum = $dataActors[actorId].meta.no_follow_switch
+                ? $dataActors[actorId].meta.no_follow_switch : -1;
         return switchNum;
     };
 
@@ -122,7 +196,41 @@ DreamX.FollowerOptions = DreamX.FollowerOptions || {};
         $gameSwitches.setValue(switchNum, true);
     };
 
+    DreamX.FollowerOptions.isFollowerOkay = function (actor) {
+        if (!actor)
+            return false;
+        if ($dataActors[actor.actorId()].meta.no_follow === '1')
+            return false;
+        var followSwitch = $dataActors[actor.actorId()].meta.no_follow_switch
+                || -1;
+        if (followSwitch !== -1 && $gameSwitches.value(followSwitch))
+            return false;
+        return true;
+    };
 
+    DreamX.FollowerOptions.isActorBattleEnabled = function (actor) {
+        if ($dataActors[actor.actorId()].meta.no_battle === '1')
+            return false;
+        var battleSwitch = $dataActors[actor.actorId()].meta.no_battle_switch
+                || -1;
+        if (battleSwitch !== -1 && $gameSwitches.value(battleSwitch))
+            return false;
+        return true;
+    };
+
+    DreamX.FollowerOptions.isActorMenuEnabled = function (actor) {
+        if ($dataActors[actor.actorId()].meta.no_menu === '1')
+            return false;
+        var menuSwitch = $dataActors[actor.actorId()].meta.no_menu_switch
+                || -1;
+        if (menuSwitch !== -1 && $gameSwitches.value(menuSwitch))
+            return false;
+        return true;
+    };
+
+    //==========================================================================
+    // Game_Followers
+    //==========================================================================
     Game_Followers.prototype.initialize = function () {
         this._visible = $dataSystem.optFollowers;
         this._gathering = false;
@@ -132,59 +240,32 @@ DreamX.FollowerOptions = DreamX.FollowerOptions || {};
         }
     };
 
+    Game_Follower.prototype.actor = function () {
+        return $gameParty.allFollowers()[this._memberIndex];
+    };
+
+    //==========================================================================
+    // Game_Party
+    //==========================================================================
+    Game_Party.prototype.allPartyMembers = function () {
+        return this._actors.map(function (id) {
+            return $gameActors.actor(id);
+        });
+    };
+
     Game_Party.prototype.allFollowers = function () {
         var followers = [];
         if (eval(parameterBattleMembersOnly)) {
-
             followers = this.battleMembers().filter(function (actor) {
                 return DreamX.FollowerOptions.isFollowerOkay(actor);
             });
         }
         else {
-            followers = this.allMembers().filter(function (actor) {
+            followers = this.allPartyMembers().filter(function (actor) {
                 return DreamX.FollowerOptions.isFollowerOkay(actor);
             });
         }
         return followers.slice(0, parseInt(eval(parameterMaxFollowers) + 1));
-    };
-
-    DreamX.FollowerOptions.isFollowerOkay = function (actor) {
-        if (!actor)
-            return false;
-        if ($dataActors[actor.actorId()].meta.no_follow === '1')
-            return false;
-        var followSwitch = $dataActors[actor.actorId()].meta.no_follow_switch || -1;
-        if (followSwitch !== -1 && $gameSwitches.value(followSwitch))
-            return false;
-        return true;
-    };
-
-    Game_Follower.prototype.actor = function () {
-        return $gameParty.allFollowers()[this._memberIndex];
-    };
-
-    Game_Party.prototype.battleMembers = function () {
-        var battleMembers = this.allMembers().filter(function (actor) {
-            return actor.isAppeared() && DreamX.FollowerOptions.isActorBattleEnabled(actor);
-        });
-        return battleMembers.slice(0, this.maxBattleMembers());
-    };
-
-    DreamX.FollowerOptions.isActorBattleEnabled = function (actor) {
-        if ($dataActors[actor.actorId()].meta.no_battle === '1')
-            return false;
-        var battleSwitch = $dataActors[actor.actorId()].meta.no_battle_switch || -1;
-        if (battleSwitch !== -1 && $gameSwitches.value(battleSwitch))
-            return false;
-        return true;
-    };
-
-    DreamX.FollowerOptions.Game_Party_leader = Game_Party.prototype.leader;
-    Game_Party.prototype.leader = function () {
-        if (eval(parameterBattleMembersOnly) === false) { 
-            return this.allMembers()[0];
-        }
-        return DreamX.FollowerOptions.Game_Party_leader.call(this);
     };
 
 })();
