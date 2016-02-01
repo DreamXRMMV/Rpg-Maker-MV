d8d1a43e74942015646b1799e3c94d0155b8d467
DreamXRMMV <faewfbaiobthwbrw5235@outlook.com>
2016-01-31 21:46:18 -0800

    Added slots

--- DreamX_SaveTarget.js
+++ DreamX_SaveTarget.js
@@ -1,15 +1,19 @@
 /*:
- * @plugindesc Save target id to a variable
+ * @plugindesc v1.1 Save target id to a variable
  *
  * <DreamX Save Target>
  * @author DreamX
  *
- * @param Default Actor Target Variable
+ * @param Default Actor ID Target Variable
  * @desc The default variable to store the id of an actor when they're targeted. Use -1 to disable. Default: -1
  * @default -1
  * 
- * @param Default Enemy Target Variable
+ * @param Default Enemy ID Target Variable
  * @desc The default variable to store the id of an enemy when they're targeted. Use -1 to disable. Default: -1
+ * @default -1
+ 
+ * @param Default Enemy Slot Target Variable
+ * @desc The default variable to store the battle slot of an enemy when they're targeted. Use -1 to disable. Default: -1
  * @default -1
  *
  * @help This plugin does not provide plugin commands.
@@ -17,8 +21,8 @@
  * How To Use
  * ============================================================================
  Put <saveTarget:1> in the notetag for a skill or item. If you want to use a 
- variable other than what was set in the plugin parameters, use <actorVariable:x>
- or <enemyVariable:x> for actor or enemy targets respectively, with x being the
+ variable other than what was set in the plugin parameters, use <actorIDVariable:x>
+ or <enemyIDVariable:x> for actor or enemy targets respectively, with x being the
  variable id. If the skill or item is successful then the id of the target will
  be stored in their respective variable.
  * ============================================================================
@@ -34,6 +38,7 @@
 //=============================================================================
 // Import
 //=============================================================================
+// Set up import so other plugins can extend this one.
 var Imported = Imported || {};
 Imported.DreamX_SaveTarget = true;
 var DreamX = DreamX || {};
@@ -41,7 +46,7 @@ DreamX.SaveTarget = DreamX.SaveTarget || {};
 
 (function () {
 
-    //=============================================================================
+//=============================================================================
 // Parameters
 //=============================================================================
     // Get the parameter list by going through the plugins and checking if the
@@ -53,9 +58,11 @@ DreamX.SaveTarget = DreamX.SaveTarget || {};
     })[0].parameters; //Thanks to Iavra
 
     // store the parameter data in variables. The defaults are -1
-    var parameterActorVariable = parseInt(parameters['Default Actor Target Variable']
+    var parameterActorIDVariable = parseInt(parameters['Default Actor ID Target Variable']
             || '-1');
-    var parameterEnemyVariable = parseInt(parameters['Default Enemy Target Variable']
+    var parameterEnemyIDVariable = parseInt(parameters['Default Enemy ID Target Variable']
+            || '-1');
+    var parameterEnemySlotVariable = parseInt(parameters['Default Enemy Slot Target Variable']
             || '-1');
     
     // Alias the Game_Action.prototype.applyItemUserEffect function
@@ -70,21 +77,25 @@ DreamX.SaveTarget = DreamX.SaveTarget || {};
         var item = this.item();
         // store the variable id to save enemy/actor id to a variable. If the 
         // notetag is not present, it uses the plugin parameter
-        var actorVariable = item.meta.actorVariable || parameterActorVariable;
-        var enemyVariable = item.meta.enemyVariable || parameterEnemyVariable;
+        var actorIDVariable = item.meta.actorIDVariable || parameterActorIDVariable;
+        var enemyIDVariable = item.meta.enemyIDVariable || parameterEnemyIDVariable;
+        var enemySlotVariable = item.meta.enemySlotVariable || parameterEnemySlotVariable;
         
         // check if in the database the item has the notetag saveTarget
         if (item.meta.saveTarget) {
             // check if the target is an enemy and enemy variable id is valid
-            if (target.isEnemy() && enemyVariable >= 1) {
-                // set the variable to the enemy's id
-                $gameVariables.setValue(enemyVariable, target.enemyId());
+            if (target.isEnemy()) {
+				// set the variable to the enemy's id
+				if (enemyIDVariable >= 1) $gameVariables.setValue(enemyIDVariable, target.enemyId());
+				
+				// set the variable to the enemy's id
+				if (enemySlotVariable >= 1) $gameVariables.setValue(enemySlotVariable, $gameTroop.allMembers().indexOf(target));
             }
             // if the target isn't an enemy, it's an actor. but we still need to
             // check if the variable id is correct
-            else if (actorVariable >= 1) {
+            else if (actorIDVariable >= 1) {
                 // store actor id to variable
-                $gameVariables.setValue(actorVariable, target.actorId());
+                $gameVariables.setValue(actorIDVariable, target.actorId());
             }
         }
     };
