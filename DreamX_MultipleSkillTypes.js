4c6c872a06bb4deb112e99e5d550aa144b3e8b01
DreamXRMMV <faewfbaiobthwbrw5235@outlook.com>
2016-03-04 23:31:55 -0800

    multiple skill types

---
+++ DreamX_MultipleSkillTypes.js
@@ -0,0 +1,42 @@
+/*:
+ * @plugindesc v0.01 Extra skill types for showing up in skill list.
+ * @author DreamX
+ * @help
+ * ============================================================================
+ * How To Use
+ * ============================================================================
+ Use this notetag on a skill: <extraSkillTypeID:x y z> with the letters being the 
+ extra skill type ids for the purposes of showing up in the skill list. You can 
+ have as many ids as you want.
+ * ============================================================================
+ * Terms Of Use
+ * ============================================================================
+ * Free to use and modify for commercial and noncommercial games, with credit.
+ * ============================================================================
+ * Credits
+ * ============================================================================
+ * DreamX
+ */
+
+ var Imported = Imported || {};
+Imported.DreamX_ExtraSkillTypes = true;
+
+var DreamX = DreamX || {};
+DreamX.ExtraSkillTypes = DreamX.ExtraSkillTypes || {};
+ 
+(function() {
+
+DreamX.ExtraSkillTypes.Window_SkillList_includes = Window_SkillList.prototype.includes;
+Window_SkillList.prototype.includes = function(item) {
+	if (item && item.meta.extraSkillTypeID) {
+		var array = item.meta.extraSkillTypeID.trim().split(" ");
+		for (var i = 0; i < array.length; i++) {
+			if (parseInt(array[i]) === this._stypeId) {
+				return true;
+			}
+		}
+	}
+    return DreamX.ExtraSkillTypes.Window_SkillList_includes.call(this, item);
+};
+
+})();
