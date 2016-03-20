b5b6ac7c95db777bdf41a9f87d0173f80387dbeb
DreamXRMMV <faewfbaiobthwbrw5235@outlook.com>
2016-03-20 04:55:13 -0700

    notes

--- DreamX_AutoPhoenix.js
+++ DreamX_AutoPhoenix.js
@@ -2,6 +2,16 @@
  * @plugindesc v0.1
  * @author DreamX
  * @help
+ * ============================================================================
+ * Terms Of Use
+ * ============================================================================
+ * Free to use and modify for commercial and noncommercial games, with credit.
+ * ============================================================================
+ * Credits & Thanks
+ * ============================================================================
+ * DreamX
+ * This plugin uses modified code snippets from Yanfly's Dash Toggle plugin. 
+ * Please remember to credit them as well.
  */
 
 var Imported = Imported || {};
@@ -38,7 +48,7 @@ DreamX.AutoPhoenix = DreamX.AutoPhoenix || {};
             if ($gameParty.hasReviveItem()) {
                 for (var i = 0; i < $gameParty.battleMembers().length; i++) {
                     var actor = $gameParty.battleMembers()[i];
-                    if (actor.isAlive() && actor.canMove() && actor.hp > 0) {
+                    if (actor.isAlive() && actor.canMove() && actor.hp > 0 && actor.isAutoPhoenixEnabled()) {
                         this.clearActions();
                         var action = new Game_Action(actor, true);
                         action.setItemObject($gameParty.reviveItems()[0]);
@@ -50,6 +60,79 @@ DreamX.AutoPhoenix = DreamX.AutoPhoenix || {};
                 }
             }
         }
+    };
+
+    // modified from yanfly's dash toggle plugin
+    DreamX.AutoPhoenix.DataManager_isDatabaseLoaded = DataManager.isDatabaseLoaded;
+    DataManager.isDatabaseLoaded = function () {
+        if (!DreamX.AutoPhoenix.DataManager_isDatabaseLoaded.call(this))
+            return false;
+        if (!DreamX._loaded_DreamX_AutoPhoenix) {
+            this.processDashNotetags1($dataActors);
+            this.processDashNotetags1($dataClasses);
+            this.processDashNotetags1($dataWeapons);
+            this.processDashNotetags1($dataArmors);
+            this.processDashNotetags1($dataStates);
+            DreamX._loaded_DreamX_AutoPhoenix = true;
+        }
+        return true;
+    };
+    
+    // modified from yanfly's dash toggle plugin
+    DataManager.processDashNotetags1 = function (group) {
+        for (var n = 1; n < group.length; n++) {
+            var obj = group[n];
+            var notedata = obj.note.split(/[\r\n]+/);
+
+            obj.enableAutoPhoenix = false;
+
+            for (var i = 0; i < notedata.length; i++) {
+                var line = notedata[i];
+                if (line.match(/<(?:AutoPhoenix)>/i)) {
+                    obj.enableAutoPhoenix = true;
+                }
+            }
+        }
+    };
+
+    // modified from yanfly's dash toggle plugin
+    DreamX.AutoPhoenix.Game_Actor_refresh = Game_Actor.prototype.refresh;
+    Game_Actor.prototype.refresh = function () {
+        this._enableAutoPhoenix = undefined;
+        DreamX.AutoPhoenix.Game_Actor_refresh.call(this);
+    };
+
+    // modified from yanfly's dash toggle plugin
+    Game_Actor.prototype.isAutoPhoenixEnabled = function () {
+        // if variable is not undefined
+        if (this._enableAutoPhoenix !== undefined)
+            return this._enableAutoPhoenix;
+        if (this.actor().enableAutoPhoenix) {
+            this._enableAutoPhoenix = true;
+            return this._enableAutoPhoenix;
+        }
+        if (this.currentClass().enableAutoPhoenix) {
+            this._enableAutoPhoenix = true;
+            return this._enableAutoPhoenix;
+        }
+        var length = this.equips().length;
+        for (var i = 0; i < length; ++i) {
+            var obj = this.equips()[i];
+            if (obj && obj.enableAutoPhoenix) {
+                this._enableAutoPhoenix = true;
+                return this._enableAutoPhoenix;
+            }
+        }
+        var length = this.states().length;
+        for (var i = 0; i < length; ++i) {
+            var obj = this.states()[i];
+            if (obj && obj.enableAutoPhoenix) {
+                this._enableAutoPhoenix = true;
+                return this._enableAutoPhoenix;
+            }
+        }
+        this._enableAutoPhoenix = false;
+        return this._enableAutoPhoenix;
     };
 
 })();
