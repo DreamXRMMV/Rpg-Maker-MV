a72033aa6f75af5256e06b08b02a2738b6377d72
DreamXRMMV <faewfbaiobthwbrw5235@outlook.com>
2016-03-31 11:30:21 -0700

    can choose weapon motion

--- DreamX_SkillMotion.js
+++ DreamX_SkillMotion.js
@@ -1,30 +1,38 @@
 /*:
- * @plugindesc v1.0 Specify skill motion.
+ * @plugindesc v1.1 Specify skill motion.
  * @author DreamX
  * @help
- Put <OverrideMotion:x> with x as the motion to use for the skill. Or, use 
-<WeaponMotion:1> to use the default weapon skill motion, like swing when you 
-use a sword.
+ You can use one of the following notetags on a skill:
+ <OverrideMotion:x> with x as the motion to use for the skill. Does not include 
+ weapon sprites.
+ 
+ <WeaponMotion:1> to use the default attack motion, which is based on your 
+ equipped weapon.
 
+ <SpecificWeaponMotion:x> with x as the weapon type id (from the Types tab in 
+ the database). It will use the weapon sprite and motion you specified for that 
+ weapon type. You can check/set the motion and sprite in the System tab in the 
+ database.
+ 
  Here is a list of the default motions:
-    walk
-    wait
-    chant
-    guard
-    damage
-    evade
-    thrust
-    swing
-    missile
-    skill
-    spell
-    item
-    escape
-    victory
-    dying
-    abnormal
-    sleep
-    dead
+ walk
+ wait
+ chant
+ guard
+ damage
+ evade
+ thrust
+ swing
+ missile
+ skill
+ spell
+ item
+ escape
+ victory
+ dying
+ abnormal
+ sleep
+ dead
  * ============================================================================
  * Terms Of Use
  * ============================================================================
@@ -45,13 +53,25 @@ DreamX.SkillMotion = DreamX.SkillMotion || {};
     DreamX.SkillMotion.Game_Actor_performAction = Game_Actor.prototype.performAction;
     Game_Actor.prototype.performAction = function (action) {
         if (action && action.item()) {
-            if (action.item().meta.OverrideMotion || action.item().meta.WeaponMotion) {
+            var meta = action.item().meta;
+            if (meta.OverrideMotion || meta.WeaponMotion || meta.SpecificWeaponMotion) {
                 Game_Battler.prototype.performAction.call(this, action);
-                if (action.item().meta.OverrideMotion) {
-                    this.requestMotion(action.item().meta.OverrideMotion);
-                }
-                else if (action.item().meta.WeaponMotion) {
+                if (meta.OverrideMotion) {
+                    this.requestMotion(meta.OverrideMotion);
+                } else if (meta.WeaponMotion) {
                     this.performAttack();
+                } else if (meta.SpecificWeaponMotion) {
+                    var attackMotion = $dataSystem.attackMotions[meta.SpecificWeaponMotion];
+                    if (attackMotion) {
+                        if (attackMotion.type === 0) {
+                            this.requestMotion('thrust');
+                        } else if (attackMotion.type === 1) {
+                            this.requestMotion('swing');
+                        } else if (attackMotion.type === 2) {
+                            this.requestMotion('missile');
+                        }
+                        this.startWeaponAnimation(attackMotion.weaponImageId);
+                    }
                 }
                 return;
             }
