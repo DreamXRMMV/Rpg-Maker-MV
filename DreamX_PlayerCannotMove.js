a2e921796dc8f9aa557f0a8cb7e10b3b4fefffed
DreamXRMMV <faewfbaiobthwbrw5235@outlook.com>
2016-05-12 12:00:42 -0700

    a

---
+++ DreamX_PlayerCannotMove.js
@@ -0,0 +1,38 @@
+/*:
+ * @plugindesc v1.0 Don't allow player movement by input if a certain switch is 
+ * on
+ * @author DreamX
+ * 
+ * @param Switch
+ * @desc The switch ID that, if on, disables player movement by input. 0: Disable Default: 0
+ * @default 0
+ * @help
+ * ============================================================================
+ * Terms Of Use
+ * ============================================================================
+ * Free to use and modify for commercial and noncommercial games, with credit.
+ * ============================================================================
+ * Credits
+ * ============================================================================
+ * DreamX
+ */
+
+var Imported = Imported || {};
+Imported.DreamX_PlayerCannotMove = true;
+
+var DreamX = DreamX || {};
+DreamX.PlayerCannotMove = DreamX.PlayerCannotMove || {};
+
+(function () {
+    var parameters = PluginManager.parameters('DreamX_PlayerCannotMove');
+    var paramSwitch = parseInt(parameters['Switch'] || 0);
+
+    DreamX.PlayerCannotMove.Game_Player_moveByInput = Game_Player.prototype.moveByInput;
+    Game_Player.prototype.moveByInput = function () {
+        if (paramSwitch >= 1 && $gameSwitches.value(paramSwitch)) {
+            return;
+        }
+        DreamX.PlayerCannotMove.Game_Player_moveByInput.call(this);
+    };
+
+})();
