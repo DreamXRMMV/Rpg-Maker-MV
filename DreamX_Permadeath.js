6637559ad86f18832903dfc6404041b708084643
DreamXRMMV <faewfbaiobthwbrw5235@outlook.com>
2016-07-03 15:57:07 -0700

    permadeath

--- DreamX_Permadeath.js
+++ DreamX_Permadeath.js
@@ -1,5 +1,5 @@
 /*:
- * @plugindesc v1.0 Adds permadeath.
+ * @plugindesc v1.1 Adds permadeath.
  * @author DreamX
  * 
  * @param Permadeath Switch
@@ -19,6 +19,9 @@
  * 
  * Use <NoPermadeathSwitch: x> with x as the switch id to specify a switch 
  * that when on, disables permadeath for the actor.
+ * 
+ * Use <NoPermadeathVariable: x> with x as the variable id to specify a 
+ * variable that when greater than 0, disables permadeath for the actor.
  * ============================================================================
  * Terms Of Use
  * ============================================================================
@@ -40,38 +43,59 @@ DreamX.Permadeath = DreamX.Permadeath || {};
     var paramPermadeathSwitchId = parseInt(parameters['Permadeath Switch']);
     var paramDisableLeaderPermadeath = eval(parameters['Disable Leader Permadeath']);
 
-    DreamX.Permadeath.BattleManager_updateBattleEnd = BattleManager.updateBattleEnd;
-    BattleManager.updateBattleEnd = function () {
-        DreamX.Permadeath.BattleManager_updateBattleEnd.call(this);
-        if (!paramPermadeathSwitchId || paramPermadeathSwitchId <= 0) {
+    BattleManager.checkPermadeath = function () {
+        if (this._canLose || !paramPermadeathSwitchId
+                || paramPermadeathSwitchId <= 0) {
             return;
         }
         if (!$gameSwitches.value(paramPermadeathSwitchId)) {
             return;
         }
-        
+
         var leader = $gameParty.leader();
         for (var i = 0; i < $gameParty.battleMembers().length; i++) {
             var battler = $gameParty.battleMembers()[i];
+
+            // ignore non-dead battlers
+            if (!battler.isDead()) {
+                continue;
+            }
+
             var dataBattler = battler.actor();
             var noPermadeathSwitch = dataBattler.meta.NoPermadeathSwitch;
-            
+            var noPermadeathVariable = dataBattler.meta.NoPermadeathVariable;
+
             if (dataBattler.meta.NoPermadeath) {
                 continue;
             }
             if (paramDisableLeaderPermadeath && battler === leader) {
                 continue;
             }
-            if (noPermadeathSwitch 
+
+            if (noPermadeathSwitch
                     && $gameSwitches.value(parseInt(noPermadeathSwitch))) {
                 continue;
             }
-            
-            var id = battler.actorId();
-            if (battler.isDead()) {
-                $gameParty.removeActor(id);
+
+            if (noPermadeathVariable) {
+                var variableId = parseInt(noPermadeathVariable);
+                var currentValue = $gameVariables.value(variableId);
+                var newValue = currentValue - 1;
+                $gameVariables.setValue(variableId, newValue);
+                if (newValue >= 0) {
+                    continue;
+                }
             }
+
+            var id = battler.actorId();
+            $gameParty.removeActor(id);
         }
+    };
+
+    DreamX.Permadeath.BattleManager_updateBattleEnd = BattleManager.updateBattleEnd;
+    BattleManager.updateBattleEnd = function () {
+        DreamX.Permadeath.BattleManager_updateBattleEnd.call(this);
+        this.checkPermadeath();
     };
 
 })();
