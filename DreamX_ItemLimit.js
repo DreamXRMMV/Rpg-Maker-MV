615ec7be2f440c119fe627033e88151efcea5051
DreamXRMMV <faewfbaiobthwbrw5235@outlook.com>
2016-07-06 23:18:02 -0700

    limit

---
+++ DreamX_ItemLimit.js
@@ -0,0 +1,38 @@
+/*:
+ * @plugindesc v1.0
+ * @author DreamX
+ * @help
+ * ============================================================================
+ * How To Use
+ * ============================================================================
+ * Place <ItemLimit: x> with x as the limit as an item notetag.
+ * ============================================================================
+ * Terms Of Use
+ * ============================================================================
+ * Free to use and modify for commercial and noncommercial games, with credit.
+ * ============================================================================
+ * Credits
+ * ============================================================================
+ * DreamX
+ */
+
+var Imported = Imported || {};
+Imported.DreamX_ItemLimit = true;
+
+var DreamX = DreamX || {};
+DreamX.ItemLimit = DreamX.ItemLimit || {};
+
+(function () {
+    DreamX.ItemLimit.Game_Party_gainItem = Game_Party.prototype.gainItem;
+    Game_Party.prototype.gainItem = function (item, amount, includeEquip) {
+        if (item) {
+            var limit = item.meta.ItemLimit;
+            if (limit) {
+                limit = parseInt(limit);
+                var current = $gameParty.numItems(item);
+                amount = Math.max(0, Math.min(amount, limit - current));
+            }
+        }
+        DreamX.ItemLimit.Game_Party_gainItem.call(this, item, amount, includeEquip);
+    };
+})();
