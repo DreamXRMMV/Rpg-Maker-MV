9fea9bc491e8aa1deff4859b5588186c8794b2a7
DreamXRMMV <faewfbaiobthwbrw5235@outlook.com>
2016-07-14 21:31:20 -0700

    params

--- DreamX_ShowParam.js
+++ DreamX_ShowParam.js
@@ -1,15 +1,41 @@
 /*:
- * @plugindesc v0.1
+ * @plugindesc v0.2
+ *
+ * @param --Status Menu Core--
+ *
+ * @param General Parameters
+ * @desc Parameters in General section Default: lvl lo 0 lo 1 lo 2 sh 3 sh 4 sh 5 sh 6 sh 7 sh
+ * @default lvl lo 0 lo 1 lo 2 sh 3 sh 4 sh 5 sh 6 sh 7 sh
  * 
+ * @param Show General Parameters
+ * @desc Whether to show General parameters Default: true
+ * @default true
+ * 
+ * @param Show General EXP
+ * @desc Whether to show EXP. Default: true
+ * @default true
+ * 
+ * @param Parameter Graph Parameters
+ * @desc Parameters to show when equipping in this order. Default: 2 3 4 5 6 7
+ * @default 2 3 4 5 6 7
+ *
  * @param --Equip Core--
- *  
+ *
  * @param Equip Parameters
  * @desc Parameters to show when equipping in this order. Default: 0 1 2 3 4 5 6 7
  * @default 0 1 2 3 4 5 6 7
- * 
+ *
  * @param SParams & XParams as %
  * @desc Show sparams and xparams as percentages. Default: true
  * @default true
+ * 
+ * @param --Item Core--
+ *
+ * @param Equip Parameters (Item Core)
+ * @desc Parameters to show when equipping in this order. Default: 0 1 2 3 4 5 6 7
+ * @default 0 1 2 3 4 5 6 7
+ * 
+ * @param --Text Manager--
  * 
  * @param hit Name
  * @desc The text name used for this attribute.
@@ -90,16 +116,18 @@
  * @param exr Name
  * @desc The text name used for this attribute.
  * @default Experience Rate
- * 
+ *
  * @author DreamX
  * @help
  * ============================================================================
  * How To Use
  * ============================================================================
- * When defining parameters, you can either use the parameter number or 
- * parameter abbreviation. If you are using Quasi Params Plus, you can use the 
+ * When defining parameters, you can either use the parameter number or
+ * parameter abbreviation. If you are using Quasi Params Plus, you can use the
  * abbreviations from that plugin as well.
  * 
+ * For Parameter "General Parameters", use lo or hi after each parameter to 
+ * designate whether is displayed with a long box or a short one.
  * ============================================================================
  * Terms Of Use
  * ============================================================================
@@ -121,7 +149,14 @@ DreamX.ShowParam = DreamX.ShowParam || {};
 (function () {
     var parameters = PluginManager.parameters('DreamX_ShowParam');
     var paramEquipParams = String(parameters['Equip Parameters']).split(" ");
+    
+    var paramEquipParamsItemCore = String(parameters['Equip Parameters (Item Core)']).split(" ");
+    
+    var paramGraphParams = String(parameters['Parameter Graph Parameters']).split(" ");
+    var paramGeneralParams = String(parameters['General Parameters']).split(" ");
     var paramShowXSParamsPercent = eval(String(parameters['SParams & XParams as %']));
+    var paramShowGenParam = eval(String(parameters['Show General Parameters']));
+    var paramShowStatusExp = eval(String(parameters['Show General EXP']));
 
     var paramHitName = String(parameters['hit Name']);
     var paramEvaName = String(parameters['eva Name']);
@@ -265,6 +300,235 @@ DreamX.ShowParam = DreamX.ShowParam || {};
         return DreamX.ShowParam.Game_BattlerBase_param.call(this, paramId);
     };
 
+    if (Imported.YEP_StatusMenuCore) {
+        Window_StatusInfo.prototype.DXGraphParams = function () {
+            var params = [];
+
+            for (var i = 0; i < paramGraphParams.length; i++) {
+                var param = paramGraphParams[i];
+                if (new RegExp("^[0-9]+$").test(param)) {
+                    params.push(parseInt(param));
+                } else {
+                    params.push(paramGraphParams[i]);
+                }
+            }
+
+            return params;
+        };
+
+        Window_StatusInfo.prototype.DXGeneralParams = function () {
+            var params = [];
+
+            for (var i = 0; i < paramGeneralParams.length; i++) {
+                var param = paramGeneralParams[i];
+                if (new RegExp("^[0-9]+$").test(param)) {
+                    params.push(parseInt(param));
+                } else {
+                    params.push(param);
+                }
+            }
+
+            return params;
+        };
+
+        Window_StatusInfo.prototype.drawParameters = function () {
+            var dx = 0;
+            var dy = this.lineHeight() / 2;
+            var dw = this.contents.width;
+            var dh = this.lineHeight();
+            var dw2;
+            var text;
+            this.changeTextColor(this.systemColor());
+            this.drawText(Yanfly.Param.StatusGraphText, dx, dy, dw, 'center');
+            dy = this.lineHeight();
+            dx = this.standardPadding();
+            dw -= this.standardPadding() * 2;
+
+
+            for (var i = 0; i < this.DXGraphParams().length; i++) {
+                var param = this.DXGraphParams()[i];
+
+                dy += this.lineHeight();
+                var rate = this.drawParamGauge(dx, dy, dw, param);
+                this.changeTextColor(this.systemColor());
+                this.drawText(TextManager.param(param), dx + 4, dy, dw - 4);
+                text = Yanfly.Util.toGroup(this._actor.param(param));
+                this.changeTextColor(this.normalColor());
+                dw2 = dw * rate;
+                this.drawText(text, dx, dy, dw2 - 4, 'right');
+            }
+        };
+
+        Window_StatusInfo.prototype.drawParamGauge = function (dx, dy, dw, paramId) {
+            var rate = this.calcParamRate(paramId);
+            var array;
+            var color1;
+            var color2;
+
+            if (paramId >= 2 && paramId <= 7) {
+                array = eval('Yanfly.Param.ColorParam' + paramId + 'Gauge').split(' ');
+                color1 = array[0];
+                color2 = array[1];
+            } else {
+                color1 = "#ed1c24";
+                color2 = "#f26c4f";
+            }
+
+            this.drawGauge(dx, dy, dw, rate, color1, color2);
+            return rate;
+        };
+
+        Window_StatusInfo.prototype.drawGeneral = function () {
+            var dx = 0;
+            var dy = this.lineHeight() / 2;
+
+            var headerWidth = (this.contents.width - this.standardPadding());
+
+            this.changeTextColor(this.systemColor());
+
+            if (paramShowGenParam && paramShowStatusExp) {
+                headerWidth /= 2;
+            }
+
+            if (paramShowGenParam) {
+                this.drawText(Yanfly.Param.StatusParamText, dx, dy, headerWidth, 'center');
+            }
+
+            if (paramShowGenParam && paramShowStatusExp) {
+                dx += this.contents.width / 2;
+            }
+
+            if (paramShowStatusExp) {
+                this.drawText(Yanfly.Param.StatusExpText, dx, dy, headerWidth, 'center');
+            }
+
+            if (paramShowGenParam) {
+                this.drawGeneralParam();
+            }
+
+            if (paramShowStatusExp) {
+                this.drawGeneralExp();
+            }
+
+        };
+
+        Window_StatusInfo.prototype.drawGeneralExp = function () {
+            var dx = 0;
+            var dy = this.lineHeight() * 2;
+            var dw = (this.contents.width - this.textPadding());
+
+            if (paramShowGenParam) {
+                dw /= 2;
+                dx = this.textPadding() + dw;
+            } else {
+                dw -= this.textPadding();
+            }
+
+            // Current Exp
+            this.changeTextColor(this.systemColor());
+            text = TextManager.expTotal.format(TextManager.exp);
+            this.drawText(text, dx, dy, dw, 'left');
+            dy += this.lineHeight();
+            this.changeTextColor(this.normalColor());
+            text = Yanfly.Util.toGroup(this._actor.currentExp());
+            this.drawText(text, dx, dy, dw, 'right');
+            // To Next Level
+            dy += this.lineHeight();
+            this.changeTextColor(this.systemColor());
+            text = TextManager.expNext.format(TextManager.level);
+            this.drawText(text, dx, dy, dw, 'left');
+            dy += this.lineHeight();
+            var rect = new Rectangle();
+            rect.x = dx;
+            rect.y = dy;
+            rect.width = dw;
+            var rate = this.actorCurrentExpRate(this._actor);
+            this.drawExpGauge(this._actor, rate, rect);
+            this.changeTextColor(this.normalColor());
+            text = Yanfly.Util.toGroup(this._actor.nextRequiredExp());
+            if (this._actor.isMaxLevel())
+                text = '-------';
+            this.drawText(text, dx, dy, dw, 'right');
+            // Total EXP for Next Level
+            dy += this.lineHeight();
+            this.changeTextColor(this.systemColor());
+            text = Yanfly.Param.StatusTotalFmt.format(TextManager.exp,
+                    TextManager.level);
+            this.drawText(text, dx, dy, dw, 'left');
+            dy += this.lineHeight();
+            var rect = new Rectangle();
+            rect.x = dx;
+            rect.y = dy;
+            rect.width = dw;
+            var rate = this.actorExpRate(this._actor);
+            this.drawExpGauge(this._actor, rate, rect);
+            this.changeTextColor(this.normalColor());
+            text = Yanfly.Util.toGroup(this._actor.nextLevelExp());
+            if (this._actor.isMaxLevel())
+                text = '-------';
+            this.drawText(text, dx, dy, dw, 'right');
+        };
+
+        Window_StatusInfo.prototype.drawGeneralParam = function () {
+            var rect = new Rectangle();
+            var width = this.contents.width - (this.standardPadding() * 2);
+            var paramsThisLine = 2;
+            var longWasLast = false;
+            var paramNameText = "";
+            var paramValueText = "";
+
+            if (paramShowStatusExp) {
+                width /= 2;
+            }
+
+            rect.width = width;
+
+            rect.y = this.lineHeight();
+            rect.height = this.lineHeight();
+
+            for (var i = 0; i < this.DXGeneralParams().length; i++) {
+                var param = this.DXGeneralParams()[i];
+                var type = this.DXGeneralParams()[i + 1];
+
+                i += 1;
+
+                if (type === 'lo' || paramsThisLine === 2 || longWasLast) {
+                    paramsThisLine = 0;
+                    rect.y += this.lineHeight();
+                    rect.x = 0;
+                }
+
+                if (type === 'sh') {
+                    rect.width = width / 2;
+                    rect.x += paramsThisLine * rect.width;
+                    paramsThisLine++;
+                    longWasLast = false;
+                } else {
+                    rect.width = width;
+                    longWasLast = true;
+                }
+
+                var dx = rect.x + this.textPadding();
+                var dw = rect.width - this.textPadding() * 2;
+
+                this.drawDarkRect(rect.x, rect.y, rect.width, rect.height);
+
+                if (param !== 'lvl') {
+                    paramNameText = TextManager.param(param);
+                    paramValueText = Yanfly.Util.toGroup(this._actor.param(param));
+                } else {
+                    paramNameText = TextManager.level;
+                    paramValueText = Yanfly.Util.toGroup(this._actor.level);
+                }
+
+                this.changeTextColor(this.systemColor());
+                this.drawText(paramNameText, dx, rect.y, dw, 'left');
+                this.changeTextColor(this.normalColor());
+                this.drawText(paramValueText, dx, rect.y, dw, 'right');
+            }
+        };
+    }
+
     if (Imported.YEP_EquipCore) {
         Window_StatCompare.prototype.DXParams = function () {
             var params = [];
@@ -290,6 +554,67 @@ DreamX.ShowParam = DreamX.ShowParam || {};
             }
         };
     }
+
+    if (Imported.YEP_ItemCore) {
+        Window_ItemStatus.prototype.DXParamsEquip = function () {
+            var params = [];
+
+            for (var i = 0; i < paramEquipParamsItemCore.length; i++) {
+                var param = paramEquipParamsItemCore[i];
+                if (new RegExp("^[0-9]+$").test(param)) {
+                    params.push(parseInt(param));
+                } else {
+                    params.push(param);
+                }
+            }
+
+            return params;
+        };
+
+        Window_ItemStatus.prototype.drawEquipInfo = function (item) {
+            var rect = new Rectangle();
+            if (eval(Yanfly.Param.ItemShowIcon)) {
+                rect.width = (this.contents.width - Window_Base._faceWidth) / 2;
+            } else {
+                rect.width = this.contents.width / 2;
+            }
+            var params = this.DXParamsEquip();
+            console.log(params);
+            
+            for (var i = 0; i < params.length; ++i) {
+                var param = params[i];
+                var paramValue = item.params[i];
+                
+                rect = this.getRectPosition(rect, i);
+                var dx = rect.x + this.textPadding();
+                var dw = rect.width - this.textPadding() * 2;
+                
+                this.changeTextColor(this.systemColor());
+                this.drawText(TextManager.param(param), dx, rect.y, dw);
+                
+                if (!new RegExp("^[0-9]+$").test(param)) {
+                    var qParamIndex = 0;
+                    QuasiParams._custom.forEach(function(qParam) {
+                        if (qParam.abr === param) {
+                            return;
+                        }
+                        qParamIndex++;
+                    });
+                    paramValue = parseInt(QuasiParams.equipParamsPlus(item)[qParamIndex + 17]);
+                }
+                this.changeTextColor(this.paramchangeTextColor(paramValue));
+                
+                var text = Yanfly.Util.toGroup(paramValue);
+                if (paramValue >= 0)
+                    text = '+' + text;
+                if (text === '+0')
+                    this.changePaintOpacity(false);
+                this.drawText(text, dx, rect.y, dw, 'right');
+                this.changePaintOpacity(true);
+            }
+        };
+    }
+
 
 
 })();
