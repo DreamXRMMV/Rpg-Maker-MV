b83474c4a6394ff5d99c1e1ec6bed97f4d38e76f
DreamXRMMV <faewfbaiobthwbrw5235@outlook.com>
2016-07-25 19:42:51 -0700

    update

--- DreamX_Ext_BattleStatusCore.js
+++ DreamX_Ext_BattleStatusCore.js
@@ -1,5 +1,5 @@
 /*:
- * @plugindesc v1.0
+ * @plugindesc v1.1
  * @author DreamX
  * 
  * @param --Battle Status Window--
@@ -14,6 +14,10 @@
  * 
  * @param --Faces--
  *  
+ * @param Draw Face
+ * @desc Eval. Default: true
+ * @default true
+ * 
  * @param Draw Face Width
  * @desc Eval. Default: Math.min(rect.width - 8, Window_Base._faceWidth)
  * @default Math.min(rect.width - 8, Window_Base._faceWidth)
@@ -61,8 +65,8 @@
  * @default gaugeRect.y + gaugeRect.height - this.lineHeight()
  * 
  * @param MP Gauge Width
- * @desc Eval. Default: gaugeRect.width / 2
- * @default gaugeRect.width / 2
+ * @desc Eval. Default: this.getGaugesDrawn(actor) ? gaugeRect.width : gaugeRect.width / 2
+ * @default this.getGaugesDrawn(actor) ? gaugeRect.width : gaugeRect.width / 2
  * 
  * @param Draw TP Gauge
  * @desc Eval. Default: $dataSystem.optDisplayTp
@@ -145,6 +149,16 @@
  * @param Icons Width
  * @desc Eval. Default: basicAreaRect.width
  * @default basicAreaRect.width
+ *  
+ * @param --Misc--
+ * 
+ * @param Frontview Animations On Top
+ * @desc Eval. Animations on actors in frontview appear above battle hud instead of below. Default: false
+ * @default false
+ * 
+ * @param DreamX Setup Suggestion
+ * @desc Overrides several parameters Default: false
+ * @default false
  * 
  * @help
  * ============================================================================
@@ -171,6 +185,30 @@
  * The rect variable refers to the entire rectangle. It's best to use this 
  * instead of basicAreaRect, gaugeRect etc if you want to change positioning.
  * ============================================================================
+ * DreamX Setup Suggestion Parameter Info
+ * ============================================================================
+ * This parameter sets up the hud similar to my example screenshot. 
+ * It overrides parameters for these things:
+ * 
+ * Face X, Width, Height
+ * Gauge/Name X, Width
+ * 
+ * Name Y
+ * HP Gauge Y
+ * MP Gauge Y
+ * 
+ * Icons X, Y
+ * 
+ * Name Alignment
+ * 
+ * Adjust Columns set to true in YEP Battle Status Window
+ * 
+ * Therefore, you can still use the other parameters to your liking to 
+ * customize further.
+ * 
+ * You can also of course individually use the functions provided by this 
+ * plugin.
+ * ============================================================================
  * Terms Of Use
  * ============================================================================
  * Free to use and modify for commercial and noncommercial games, with credit.
@@ -181,6 +219,33 @@
  * Yanfly for YEP Battle Status Window
  */
 
+// * @param Battle Status Width
+// * @desc Eval. Default: Graphics.boxWidth - 192
+// * @default Graphics.boxWidth - 192
+// * 
+// * @param Battle Status Height
+// * @desc Eval. Default: this.fittingHeight(this.numVisibleRows())
+// * @default this.fittingHeight(this.numVisibleRows())
+
+// * @param --Actor Picture--
+// *  
+// * @param Actor Picture X
+// * @desc Eval. Default: rect.x
+// * @default rect.x
+// * 
+// * @param Actor Picture Y
+// * @desc Eval. Default: rect.y
+// * @default rect.y
+
+// * ============================================================================
+// * Actor Picture Parameters
+// * ============================================================================
+// * These parameters allow you to draw a picture in the battle hud for 
+// * each actor. It is drawn underneath just about everything else.
+// * 
+// * Use <BattleHudPicture: x> with x as the name of the picture as an actor 
+// * notetag.
+
 var Imported = Imported || {};
 Imported.DreamX_Ext_BattleStatusCore = true;
 
@@ -189,6 +254,8 @@ DreamX.Ext_BattleStatusCore = DreamX.Ext_BattleStatusCore || {};
 
 (function () {
     var parameters = PluginManager.parameters('DreamX_Ext_BattleStatusCore');
+
+    var paramDrawFace = String(parameters['Draw Face']);
     var paramDrawFaceWidth = String(parameters['Draw Face Width']);
     var paramDrawFaceHeight = String(parameters['Draw Face Height']);
     var paramDrawFaceX = String(parameters['Face X']);
@@ -228,10 +295,48 @@ DreamX.Ext_BattleStatusCore = DreamX.Ext_BattleStatusCore || {};
     var paramIconsY = String(parameters['Icons Y']);
     var paramIconsWidth = String(parameters['Icons Width']);
 
-    //var paramDirectGaugeY = eval(String(parameters['Direct Gauge Y Placement']));
-
     var paramFrameOpacity = parseInt(String(parameters['Battle Status Frame Opacity']));
     var paramWindowOpacity = parseInt(String(parameters['Battle Status Window Opacity']));
+
+    var paramDXStyle = eval(String(parameters['DreamX Setup Suggestion']));
+
+    var paramActorPictureX = String(parameters['Actor Picture X']);
+    var paramActorPictureY = String(parameters['Actor Picture Y']);
+
+    var paramStatusWidth = String(parameters['Battle Status Width']);
+    var paramStatusHeight = String(parameters['Battle Status Width']);
+    
+    var paramTopAnimations = String(parameters['Frontview Animations On Top']);
+    
+
+    //
+
+    if (paramDXStyle) {
+        paramDrawFaceX = "rect.x";
+        paramDrawFaceWidth = "144";
+        paramDrawFaceHeight = "144";
+
+        paramHpGaugeX = "this.DXGaugeNameX(actor, rect)";
+        paramMpGaugeX = "this.DXGaugeNameX(actor, rect)";
+        paramTpGaugeX = "this.DXGaugeNameX(actor, rect)";
+        paramAtbGaugeX = "this.DXGaugeNameX(actor, rect)";
+        paramNameX = "this.DXGaugeNameX(actor, rect)";
+
+        paramHpGaugeWidth = "this.DXGaugeNameWidth(actor, rect)";
+        paramMpGaugeWidth = "this.DXGaugeNameWidth(actor, rect)";
+        paramTpGaugeWidth = "this.DXGaugeNameWidth(actor, rect)";
+        paramAtbGaugeWidth = "this.DXGaugeNameWidth(actor, rect)";
+        paramNameWidth = "this.DXGaugeNameWidth(actor, rect)";
+
+        paramHpGaugeY = "rect.y + this.lineHeight()";
+        paramMpGaugeY = "rect.y + this.lineHeight() * 2";
+        paramNameY = "rect.y";
+        paramIconsX = "this.DXIconX(actor, rect)";
+        paramIconsY = "rect.y + rect.height - Window_Base._iconHeight - this.textPadding()";
+        Yanfly.Param.BSWAdjustCol = "true";
+
+        paramNameAlign = "center";
+    }
 
     DreamX.Ext_BattleStatusCore.Window_BattleStatus_initialize = Window_BattleStatus.prototype.initialize;
     Window_BattleStatus.prototype.initialize = function () {
@@ -240,6 +345,14 @@ DreamX.Ext_BattleStatusCore = DreamX.Ext_BattleStatusCore || {};
         this.opacity = paramWindowOpacity;
     };
 
+//    Window_BattleStatus.prototype.windowWidth = function () {
+//        return eval(paramStatusWidth);
+//    };
+//    
+//    Window_BattleStatus.prototype.windowHeight = function () {
+//        return eval(paramStatusHeight);
+//    };
+
     Window_BattleStatus.prototype.drawStatusFace = function (index) {
         var actor = $gameParty.battleMembers()[index];
         var rect = this.itemRect(index);
@@ -247,13 +360,34 @@ DreamX.Ext_BattleStatusCore = DreamX.Ext_BattleStatusCore || {};
         var wh = eval(paramDrawFaceHeight);
         var wx = eval(paramDrawFaceX);
         var wy = eval(paramDrawFaceY);
+
+        if (!eval(paramDrawFace)) {
+            return;
+        }
+
         this.drawActorFace(actor, wx, wy, ww, wh);
     };
 
     Window_BattleStatus.prototype.drawItem = function (index) {
         var actor = $gameParty.battleMembers()[index];
+        //this.drawActorPicture(actor, index);
         this.drawBasicArea(this.basicAreaRect(index), actor, index);
         this.drawGaugeArea(this.gaugeAreaRect(index), actor, index);
+    };
+
+    Window_BattleStatus.prototype.drawActorPicture = function (actor, index) {
+        var pictureName = actor.actor().meta.BattleHudPicture;
+        var rect = this.itemRect(index);
+
+        if (!pictureName) {
+            return;
+        }
+        pictureName = pictureName.trim();
+
+        var picture = ImageManager.loadPicture(pictureName);
+
+        this.contents.blt(picture, 0, 0, picture.width, picture.height,
+                eval(paramActorPictureX), eval(paramActorPictureY));
     };
 
     Window_BattleStatus.prototype.drawGaugeArea = function (gaugeRect, actor, index) {
@@ -310,7 +444,7 @@ DreamX.Ext_BattleStatusCore = DreamX.Ext_BattleStatusCore || {};
         var iconsX = eval(paramIconsX);
         var iconsY = eval(paramIconsY);
         var iconsWidth = eval(paramIconsWidth);
-        
+
 
         if (eval(paramDrawActionIcon)) {
             this.drawActorActionIcon(actor, actionIconX, actionIconY);
@@ -333,6 +467,76 @@ DreamX.Ext_BattleStatusCore = DreamX.Ext_BattleStatusCore || {};
                 this.drawActorAtbGauge(actor, eval(paramAtbGaugeX),
                         eval(paramAtbGaugeY), eval(paramAtbGaugeWidth));
             }
+        }
+    };
+
+    //=============================================================================
+    // DreamX Setup
+    //=============================================================================
+    Window_BattleStatus.prototype.DXIconX = function (actor, rect) {
+        var availableSpace = this.DXRectFreeSpace(rect);
+
+        var x2 = rect.x + rect.width;
+        if (availableSpace < actor.allIcons().length * Window_Base._iconWidth) {
+            return x2 - (actor.allIcons().length * Window_Base._iconWidth) - this.textPadding();
+        } else {
+            return rect.x + Window_Base._faceWidth;
+        }
+    };
+
+    Window_BattleStatus.prototype.DXIconW = function (actor, rect) {
+        var x2 = rect.x + rect.width;
+
+        if (this.DXRectFreeSpace(rect) + Window_Base._iconWidth
+                < actor.allIcons() * Window_Base._iconWidth) {
+            return x2 - (x2 - (actor.allIcons() * Window_Base._iconWidth));
+        } else {
+            return this.DXRectFreeSpace(rect) + Window_Base._iconWidth;
+        }
+    };
+
+    Window_BattleStatus.prototype.DXThreshold = function () {
+        return 64;
+    };
+
+    Window_BattleStatus.prototype.DXGaugeNameX = function (actor, rect) {
+        var availableSpace = this.DXRectFreeSpace(rect);
+
+        if (availableSpace > this.DXThreshold()) {
+            return rect.x + rect.width - (this.DXRectFreeSpace(rect));
+        } else {
+            return rect.x + rect.width - (this.DXRectFreeSpace(rect)
+                    + Window_Base._iconWidth);
+        }
+    };
+
+    Window_BattleStatus.prototype.DXGaugeNameWidth = function (actor, rect) {
+        var availableSpace = this.DXRectFreeSpace(rect);
+
+        if (availableSpace > this.DXThreshold()) {
+            return this.DXRectFreeSpace(rect) - this.textPadding();
+        } else {
+            return (this.DXRectFreeSpace(rect) - this.textPadding())
+                    + Window_Base._iconWidth;
+        }
+    };
+
+
+    Window_BattleStatus.prototype.DXRectFreeSpace = function (rect) {
+        return rect.width - Window_Base._faceWidth;
+    };
+
+    DreamX.Ext_BattleStatusCore.Spriteset_Battle_createActors = Spriteset_Battle.prototype.createActors;
+    Spriteset_Battle.prototype.createActors = function () {
+        if ($gameSystem.isSideView() || !eval(paramTopAnimations)) {
+            DreamX.Ext_BattleStatusCore.Spriteset_Battle_createActors.call(this);
+            return;
+        }
+        var scene = SceneManager._scene;
+        this._actorSprites = [];
+        for (var i = 0; i < $gameParty.maxBattleMembers(); i++) {
+            this._actorSprites[i] = new Sprite_Actor();
+            scene.addChild(this._actorSprites[i]);
         }
     };
 
