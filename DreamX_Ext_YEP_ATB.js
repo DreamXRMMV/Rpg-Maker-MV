4e1963360c57505e5b15e9147f6f4817453a37c9
DreamXRMMV <faewfbaiobthwbrw5235@outlook.com>
2016-08-20 06:08:21 -0700

    charge satte

--- DreamX_Ext_YEP_ATB.js
+++ DreamX_Ext_YEP_ATB.js
@@ -5,8 +5,17 @@
  * @param Show ATB Bar For Actor SV Sprites
  * @desc Eval. Default: true
  * @default true
+ * 
+ * @param Charge ATB Per Tick
+ * @desc Eval. Default: user.agi
+ * @default user.agi
  *
  * @help
+ * ============================================================================
+ * Skill Notetags
+ * ============================================================================
+ * Use <ChargeState: x> with x as the state id to set a state applied to the 
+ * battler while they are charging the skill.
  * ============================================================================
  * Terms Of Use
  * ============================================================================
@@ -27,6 +36,7 @@ DreamX.Ext_YEP_ATB = DreamX.Ext_YEP_ATB || {};
 (function () {
     var parameters = PluginManager.parameters('DreamX_Ext_YEP_ATB');
     var paramShowActorAtb = String(parameters['Show ATB Bar For Actor SV Sprites']);
+    var paramChargeATBPerTick = String(parameters['Charge ATB Per Tick']);
 
 
 
@@ -89,6 +99,96 @@ DreamX.Ext_YEP_ATB = DreamX.Ext_YEP_ATB || {};
                 this._visualATBWindow.setBattler(battler);
         };
     }
+
+    Game_Battler.prototype.updateATB = function () {
+        if (this.isDead())
+            return this.resetAllATB();
+
+        // charge state
+        var chargeStateId = this._chargeStateId;
+        if (chargeStateId) {
+            if (this.isATBCharging()) {
+                if (!this.isStateAffected(chargeStateId)) {
+                    this.addState(chargeStateId);
+                }
+            } else {
+                this.removeState(chargeStateId);
+            }
+        }
+
+        if (!this.canMove()) {
+            this.updateATBStates();
+            return;
+        }
+        if (this.isATBCharging()) {
+            if (!this.currentAction())
+                this.resetAllATB();
+            if (this.currentAction() && this.currentAction().item() === null) {
+                this.resetAllATB();
+            }
+        }
+
+        if (this.isATBCharging()) {
+            var value = this.atbCharge() + this.chargeAtbSpeedTick();
+            this.setATBCharge(value);
+        } else if (this.atbRate() < 1) {
+            this.removeState
+            var value = this.atbSpeed() + this.atbSpeedTick();
+            this.setATBSpeed(value);
+        }
+    };
+
+    Game_Battler.prototype.chargeAtbSpeedTick = function () {
+        var value = this.ChargeAtbTickValue();
+        if (BattleManager.atbRubberband()) {
+            var min = BattleManager.atbMinimumSpeed();
+            var max = BattleManager.atbMaximumSpeed();
+            value = value.clamp(min, max);
+        }
+        return value * BattleManager.tickRate();
+    };
+
+    Game_Battler.prototype.ChargeAtbTickValue = function () {
+        if (this._chargeAtbTickValue !== undefined)
+            return this._chargeAtbTickValue;
+        var a = this;
+        var user = this;
+        var subject = this;
+        this._chargeAtbTickValue = eval(paramChargeATBPerTick);
+        return this._chargeAtbTickValue;
+    };
+
+    DreamX.Ext_YEP_ATB.Game_BattlerBase_refresh = Game_BattlerBase.prototype.refresh;
+    Game_BattlerBase.prototype.refresh = function () {
+        DreamX.Ext_YEP_ATB.Game_BattlerBase_refresh.call(this);
+        this._chargeAtbTickValue = undefined;
+    };
+
+    DreamX.Ext_YEP_ATB.Game_Battler_setupATBCharge = Game_Battler.prototype.setupATBCharge;
+    Game_Battler.prototype.setupATBCharge = function () {
+        DreamX.Ext_YEP_ATB.Game_Battler_setupATBCharge.call(this);
+        if (this.currentAction() && this.currentAction().item()) {
+            var chargeState = this.currentAction().item().meta.ChargeState;
+            if (chargeState) {
+                console.log("hi");
+                this._chargeStateId = parseInt(chargeState.trim());
+                return;
+            }
+        }
+        this._chargeStateId = undefined;
+    };
+
+    DreamX.Ext_YEP_ATB.Game_Action_apply = Game_Action.prototype.apply;
+    Game_Action.prototype.apply = function (target) {
+        DreamX.Ext_YEP_ATB.Game_Action_apply.call(this, target);
+        var item = this.item();
+        var chargeState = item.meta.ChargeState;
+        if (chargeState) {
+            this.subject()._chargeStateId = parseInt(chargeState.trim());
+        } else {
+            this.subject()._chargeStateId = undefined;
+        }
+    };
 
 
 })();
